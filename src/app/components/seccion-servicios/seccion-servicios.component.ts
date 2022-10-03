44b6f7f5e32866cade41d935527618b00354e9cd
unknown <lauragomez010203@gmail.com>
2022-10-02 19:52:15 -0500

    servicios update

--- src/app/components/seccion-servicios/seccion-servicios.component.ts
+++ src/app/components/seccion-servicios/seccion-servicios.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ApiService } from 'src/app/services/api.service';
 
 @Component({
   selector: 'app-seccion-servicios',
@@ -6,10 +7,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./seccion-servicios.component.css']
 })
 export class SeccionServiciosComponent implements OnInit {
+  public cliente = false;
+  public controllerConductor = "Servicio/Conductor";
+  public serviciosConductor = [];
+  public controllerCliente = "Servicio/Clientes";
+  public serviciosCliente = [];
 
-  constructor() { }
+  constructor(private service: ApiService) { }
 
   ngOnInit(): void {
+    if (this.cliente) {
+      this.service.getById(this.controllerCliente, '1001329488').subscribe((resp:any) => {
+        console.log(resp);
+        this.serviciosCliente = resp;
+      })
+    } else {
+      this.service.getById(this.controllerConductor, '123456789').subscribe((resp:any) => {
+        console.log(resp);
+        this.serviciosConductor = resp;
+      })
+    }
   }
 
 }
